86ad4a63fce5c08332ee7e6855799644c540e24c
maya Geva <mayageva11@cs.colman.ac.il>
2024-01-16 19:12:32 +0200

    style app elents

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,32 @@ import './App.css';
 import ExampleComponent from './components/ExampleComponent';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <ExampleComponent />
+      <div class="nav-bar"/>
+      <div className="content"/>
     </div>
-  );
+  )
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //     <ExampleComponent />
+  //   </div>
+  // );
 }
 
 export default App;
