190f8f812151e5621b015693d6bfccb4c3201870
maya Geva <mayageva11@cs.colman.ac.il>
2024-03-04 16:29:52 +0200

    conection to mongo

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
 import AboutPage from './components/AboutPage/AboutPage';
 import SignInPage from './components/SignInPage/SignInPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import HomePage from './components/HomePage/HomePage';
-import NavBar from './components/NavBar/navBar';	
+import NavBar from './components/NavBar/navBar';
 import './App.css';
+import GoogleAuth from './components/GoogleAuth/GoogleAuth';
 
 function App() {
- 
+
   return (
-    <Router> 
-      <div className="App">
-      <NavBar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/home" element={<HomePage />} />
-        </Routes>
-      </div>
-    </Router>
+    <GoogleAuth>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/signin" element={<SignInPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/home" element={<HomePage />} />
+          </Routes>
+        </div>
+      </Router>
+    </GoogleAuth>
   );
 }
 
--- client/src/components/SignInPage/SignInPage.js
+++ client/src/components/SignInPage/SignInPage.js
@@ -1,14 +1,32 @@
 // SignInPage.js
-import React from 'react';
+import React, {useState} from 'react';
+import { useGoogleLogin } from '@react-oauth/google';
+import GoogleIcon from '@mui/icons-material/Google';
+import { IconButton } from '@mui/material';
 import './SignInPage.css';
-import googleIcon from '../../assets/images/googleIcon.png'; 
-import IconButton from '@material-ui/core/IconButton';
+
+
 
 const SignInPage = () => {
+
+  const login = useGoogleLogin({
+    onSuccess: () => {console.log('Success')}
+  })
+  const [email, setEmail]= useState('');
+  const [password, setPassword]= useState('');
+
   const handleSignInWithEmail = () => {
     // Implement email/password sign-in logic here
     console.log("Signing in with email and password...");
   };
+
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value); 
+  }
+
+  const handleChangePassword = (event) =>{
+    setPassword(event.target.value);
+  }
 
   const handleSignInWithGoogle = () => {
     // Implement Google sign-in logic here
@@ -20,20 +38,18 @@ const SignInPage = () => {
       <div className="glass-container-sign-in">
         <h2>Sign In</h2>
         <form className="sign-in-form" onSubmit={handleSignInWithEmail}>
-          <input type="email" placeholder="Email" required />
-          <input type="password" placeholder="Password" required />
-          <button type="submit">Sign In</button>
+          <input  className="email-input" onChange={handleChangeEmail} value={email} type="email" placeholder="Email" required />
+          <input className="password-input" onChange={handleChangePassword} value={password} type="password" placeholder="Password" required />
+          <button className="sign-in-btn" type="submit">Sign In</button>
         </form>
         <div className="or-text">
           <span>or</span>
         </div>
         <div className="google-sign-in">
-          <span>Sign in with Google</span>
-          <IconButton onClick={handleSignInWithGoogle} aria-label="Google">
-            <img src={googleIcon} alt="Google" />
-          </IconButton>
+          <div className="google-header"  >Sign In With Google</div>
+          <IconButton sx={{width: 'fit-content'}}  onClick={login}><GoogleIcon/></IconButton>
         </div>
-        <p className="account-question">Don't have an account? <a  className="register-link" href="/register">Register</a></p>
+        <p className="account-question">Don't have an account? <a className="register-link" href="/register">Register</a></p>
       </div>
     </div>
   );
