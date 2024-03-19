709a1ef8347ac6fa90790cc5d5926b07e8ca5394
maya Geva <mayageva11@cs.colman.ac.il>
2024-03-19 18:48:24 +0200

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            "name": "server",
+            "script": "./server/app.js"
+        },
+        {
+            "name": "client",
+            "script": "./client/server.js"
+        }
+    ]
+};
