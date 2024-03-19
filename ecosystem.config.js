97e13bb8ef82b128a5571f3457add634627b2064
maya Geva <mayageva11@cs.colman.ac.il>
2024-03-19 19:06:58 +0200

    client config env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,15 @@ module.exports = {
     apps: [
         {
             "name": "server",
-            "script": "./server/app.js"
+            "script": "./server/app.js",
         },
         {
             "name": "client",
-            "script": "./client/server.js"
+            "script": "./client/server.js",
+            "env_production": {
+                "REACT_APP_SERVER_URL": "https://node09.cs.colman.ac.il",
+                "REACT_APP_SERVER_PORT": "4001"
+            }
         }
     ]
 };
